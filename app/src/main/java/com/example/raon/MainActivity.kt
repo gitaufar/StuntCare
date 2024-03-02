8f49c33b5c5f9b81dd8a278c05f04e5342443717
Aufar <zhafiraufar123@gmail.com>
2024-03-02 21:03:45 +0700

    .

--- app/src/main/java/com/example/raon/MainActivity.kt
+++ app/src/main/java/com/example/raon/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.raon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val login: AppCompatButton = findViewById(R.id.btnLogin)
+        val register: AppCompatButton = findViewById(R.id.btnRegister)
+
+        login.setOnClickListener(){
+
+        }
     }
 }
