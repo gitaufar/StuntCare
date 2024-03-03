efa720647bfb0af7c00251400e8289e0f5ab8321
Aufar <zhafiraufar123@gmail.com>
2024-03-03 10:33:51 +0700

    login

--- app/src/main/java/com/example/raon/LoginActivity.kt
+++ app/src/main/java/com/example/raon/LoginActivity.kt
@@ -1,18 +1,44 @@
 package com.example.raon
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
-    val auth = Firebase
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loginn)
         val auth = FirebaseAuth.getInstance()
         val btnLogin: Button = findViewById(R.id.login)
+        val etPass : TextInputEditText = findViewById(R.id.etPasswordLogin)
+        val etEmail: TextInputEditText = findViewById(R.id.etEmailLogin)
+        
+        btnLogin.setOnClickListener(){
+            val email: String = etEmail.text.toString()
+            val pass: String = etPass.text.toString()
+            
+            if(email.isNotEmpty() && pass.isNotEmpty()){
+                auth.signInWithEmailAndPassword(email,pass).addOnCompleteListener({
+                    if(it.isSuccessful){
+                        val intent = Intent(this,HomeActivity::class.java)
+                        Toast.makeText(this, "Login Success", Toast.LENGTH_SHORT).show()
+                      startActivity(intent)
+                    } else {
+                        Toast.makeText(this, "wrong email or password", Toast.LENGTH_SHORT).show()
+                    }
+                })
+            } else {
+                Toast.makeText(this, "email and password cant be empty", Toast.LENGTH_SHORT).show()
+            }
+
+        }
         
     }
 }
