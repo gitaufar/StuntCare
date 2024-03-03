1d0cd4bf678d62463741d4ba95d2d81dce0c9ddc
ito <lovelyito@student.ub.ac.id>
2024-03-03 10:25:04 +0700

    register

---
+++ app/src/main/java/com/example/raon/RegisterActivity.kt
@@ -0,0 +1,56 @@
+package com.example.raon
+
+
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.raon.databinding.ActivityRegisterBinding
+
+import com.google.firebase.auth.FirebaseAuth
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityRegisterBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.RBtn1.setOnClickListener{
+            val email = binding.RTxtEmail.text.toString()
+            val pass = binding.RTxtPassword.text.toString()
+            val confirmPass = binding.RTxtConfirmPassword.text.toString()
+
+            if(email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()){
+                if (pass == confirmPass){
+                    firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener{
+                        if(it.isSuccessful){
+                            val intent = Intent(this , RegisterActivity::class.java)
+                            startActivity(intent)
+                        }else{
+                            Toast.makeText(this , it.exception.toString() , Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }else{
+                    Toast.makeText(this , "Password is not matching" , Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(this , "Empty Fields Are Not Allowed !!" , Toast.LENGTH_SHORT).show()
+            }
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
