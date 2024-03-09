cfa5e62fcf837637fb30e402a392b615357fbd17
ito <lovelyito@student.ub.ac.id>
2024-03-09 11:38:15 +0700

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,8 @@ dependencies {
 
     implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
     implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+
+
 }
 
 
