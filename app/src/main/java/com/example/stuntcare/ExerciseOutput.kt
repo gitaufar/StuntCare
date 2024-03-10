6773209c2744ffd948feb7f23cdfa8c757a9725a
Aufar <zhafiraufar123@gmail.com>
2024-03-11 00:31:15 +0700

    food output,nutrition output(layout)

---
+++ app/src/main/java/com/example/stuntcare/ExerciseOutput.kt
@@ -0,0 +1,12 @@
+package com.example.stuntcare
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.raon.R
+
+class ExerciseOutput : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exercise_output)
+    }
+}
